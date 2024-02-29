d886ee91300e700e0a7852ba01192c12d41b6120
Kyle Husmann <kyle.husmann@gmail.com>
2024-02-28 16:39:54 -0800

    rework interlace columns

--- R/interlacer.R
+++ R/interlacer.R
@@ -2,6 +2,7 @@
 #' @import dplyr
 #' @import cli
 #' @import readr
+#' @importFrom purrr list_flatten
 #' @importFrom glue glue
 
 NULL
--- R/type_convert_interlaced.R
+++ R/type_convert_interlaced.R
@@ -48,23 +48,56 @@ type_convert_interlaced <- function(
 }
 
 #' @export
-interlace_columns <- function(df, default_na = "") {
-  df |>
-    transmute(
-      across(
-        !matches("\\..*\\."), function(col) {
-          missing_name <- paste0(".", cur_column(), ".")
-          if (missing_name %in% names(df)) {
-            missing_col <- df[[missing_name]]
-            case_when(
-              !is.na(col) ~ as.character(col),
-              !is.na(missing_col) ~ as.character(missing_col),
-              T ~ default_na
-            )
-          } else {
-            if_else(is.na(col), default_na, as.character(col))
-          }
-        }
-      )
+interlace_columns <- function(df) {
+  call = current_call()
+
+  cli_abort_helper <- function(msg) {
+    cli_abort(
+      c(msg, "i" = "Try running `collect_missing()`."), call = call
     )
+  }
+
+  lapply(value_names(df), function(value_name) {
+    values <- df[[value_name]]
+
+    missing_name <- to_missing_name(value_name)
+
+    if (is.null(df[[missing_name]])) {
+      cli_abort_helper(
+        glue("Missing column `{missing_name}` does not exist.")
+      )
+    }
+
+    missing_values <- df[[missing_name]]
+
+    if (any(is.na(values) & is.na(missing_values))) {
+      cli_abort_helper(
+        glue(
+          paste(
+            "Some missing values in column `{value_name}` do not have",
+            "missing reasons in `{missing_name}`."
+          )
+        )
+      )
+    }
+
+    if (any(!is.na(values) & !is.na(missing_values))) {
+      cli_abort_helper(
+        glue(
+          paste(
+            "Some non-missing values in column `{value_name}` have missing",
+            "reasons in `{missing_name}`."
+          )
+        )
+      )
+    }
+
+    if_else(
+      is.na(missing_values),
+      as.character(values),
+      as.character(missing_values),
+    )
+  }) |>
+    set_names(value_names(df)) |>
+    bind_cols()
 }
