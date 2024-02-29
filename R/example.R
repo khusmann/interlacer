ef2136254efb9c8759ca1c291358bc2e90849e3d
Kyle Husmann <kyle.husmann@gmail.com>
2024-02-29 13:58:36 -0800

    Begin work on Rmd readme

---
+++ R/example.R
@@ -0,0 +1,17 @@
+#' Get path to interlacer example
+#'
+#' interlacer comes bundled with a number of sample files in its `inst/extdata`
+#' directory. This function make them easy to access
+#'
+#' @inheritParams readr::readr_example
+#' @export
+#' @examples
+#' interlacer_example()
+#' interlacer_example("basic.csv")
+interlacer_example <- function(file = NULL) {
+  if (is.null(file)) {
+    dir(system.file("extdata", package = "interlacer"))
+  } else {
+    system.file("extdata", file, package = "interlacer", mustWork = TRUE)
+  }
+}
