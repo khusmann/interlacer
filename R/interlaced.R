7a7488c414ebfc1f4ae1f3d286e389eeebb3814b
Kyle Husmann <kyle.husmann@gmail.com>
2024-06-14 15:06:40 -0700

    update parse_interlaced to work with new collector system

--- R/interlaced.R
+++ R/interlaced.R
@@ -60,24 +60,30 @@ is.interlaced <- function(x) {
 #' by parsing it with a readr `collector` type.
 #'
 #' @param x A character vector
-#' @param na A vector of values to interpret as missing values
-#' @param .value_col A collector to parse the character values (e.g.
-#' `readr::col_double()`, `readr::col_integer()`, etc.)
+#' @param na A missing reason collector (e.g. `na_col_integer()`), or one of
+#' its shortcuts (e.g. a list of missing values)
+#' @param .default A value collector to parse the character values (e.g.
+#' `v_col_double()`, `v_col_integer()`, etc.)
 #'
 #' @returns An `interlaced` vector
 #'
 #' @export
 parse_interlaced <- function(
   x, na,
-  .value_col = readr::col_guess()
+  .default = v_col_guess()
 ) {
   if (!is.character(x)) {
     cli_abort("{.arg x} must be a character vector")
   }
 
-  v <- type_convert_col(x, .value_col, na = as.character(na))
+  value_collector <- as.value_collector(.default)
+  na_collector <- as.na_collector(na)
 
-  m <- na[match(x, na)]
+  v <- value_collector$value_fn(
+    type_convert_col(x, value_collector$impl, na = na_collector$chr_values)
+  )
+
+  m <- na_collector$values[match(x, na_collector$chr_values)]
 
   new_interlaced(v, m)
 }
