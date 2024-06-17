e376a7dead617276094802a0da4d04029dba677a
Kyle Husmann <kyle.husmann@gmail.com>
2024-06-17 11:48:49 -0700

    move interlaced tidy helpers to their own file

--- R/interlaced.R
+++ R/interlaced.R
@@ -828,53 +828,6 @@ as.ordered.interlacer_interlaced <- function(x, ...) {
   as.ordered(value_channel(x), ...)
 }
 
-# Tidy helpers
-
-#' @export
-where_value_channel <- function(fn) {
-  tidyselect::where(\(x) fn(value_channel(x)))
-}
-
-#' @export
-where_na_channel <- function(fn) {
-  tidyselect::where(\(x) fn(na_channel(x)))
-}
-
-#' @export
-across_value_channels <- function(.cols, .fns, .names = NULL, .unpack = FALSE) {
-  across_lift(map_value_channel, {{.cols}}, {{.fns}}, .names, .unpack)
-}
-
-#' @export
-across_na_channels <- function(.cols, .fns, .names = NULL, .unpack = FALSE) {
-  across_lift(map_na_channel, {{.cols}}, {{.fns}}, .names, .unpack)
-}
-
-across_lift <- function(
-  lift_fn, .cols, .fns, .names, .unpack, error_call = caller_env()
-) {
-  .fns <- eval_tidy({{.fns}})
-
-  do_lift <- function(x) {
-    if (is_formula(x) || is_function(x)) {
-      \(w) lift_fn(w, as_function(x, arg = ".fns", call = error_call))
-    } else {
-      abort(
-        "`.fns` must be a function, a formula, or a list of functions/formulas.",
-        call = error_call
-      )
-    }
-  }
-
-  if (is_list(.fns)) {
-    wrapped_fns <- map(.fns, do_lift)
-  } else {
-    wrapped_fns <- do_lift(.fns)
-  }
-
-  dplyr::across({{.cols}}, wrapped_fns, .names = .names, .unpack = .unpack)
-}
-
 # Helpers -----------------------------------------------------------------
 
 stop_unsupported <- function(x, method) {
